2aa2c8c4b82a0afac5a29a2d19a053bb28d0fd55
ProDesignV2 <eoinpgogarty@gmail.com>
2019-06-04 21:02:56 +0100

    Authentication added with saved details after initial login

---
+++ app/src/main/java/com/cbr/cbrstaff/login/LoginActivity.java
@@ -0,0 +1,136 @@
+package com.cbr.cbrstaff.login;
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.cbr.cbrstaff.Outstanding;
+import com.cbr.cbrstaff.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private LoginViewModel loginViewModel;
+    private FirebaseAuth firebaseAuth;
+    private SharedPreferences loginDetails;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
+                .get(LoginViewModel.class);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        final EditText usernameEditText = findViewById(R.id.username);
+        final EditText passwordEditText = findViewById(R.id.password);
+        final Button loginButton = findViewById(R.id.login);
+        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+
+        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
+            @Override
+            public void onChanged(@Nullable LoginFormState loginFormState) {
+                if (loginFormState == null) {
+                    return;
+                }
+                loginButton.setEnabled(loginFormState.isDataValid());
+                if (loginFormState.getUsernameError() != null) {
+                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
+                }
+                if (loginFormState.getPasswordError() != null) {
+                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
+                }
+            }
+        });
+
+        TextWatcher afterTextChangedListener = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // ignore
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // ignore
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
+                        passwordEditText.getText().toString());
+            }
+        };
+        usernameEditText.addTextChangedListener(afterTextChangedListener);
+        passwordEditText.addTextChangedListener(afterTextChangedListener);
+        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    loginButton.callOnClick();
+                }
+                return false;
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadingProgressBar.setVisibility(View.VISIBLE);
+                final String username = usernameEditText.getText().toString();
+                final String password = passwordEditText.getText().toString();
+                firebaseAuth.signInWithEmailAndPassword(username, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                loadingProgressBar.setVisibility(View.GONE);
+                                if(task.isSuccessful()){ updateUiWithUser(username, password); }
+                                else { showLoginFailed(); }
+                            }
+                        });
+            }
+        });
+
+        loginDetails = this.getSharedPreferences("Login",0);
+        String savedUsername = loginDetails.getString("Username", null);
+        String savedPassword = loginDetails.getString("Password", null);
+
+        if(savedUsername != null){ usernameEditText.setText(savedUsername); }
+        if(savedPassword != null){ passwordEditText.setText(savedPassword); }
+    }
+
+    private void updateUiWithUser(String username, String password) {
+        SharedPreferences.Editor spEditor = loginDetails.edit();
+        spEditor.putString("Username", username);
+        spEditor.putString("Password", password);
+        spEditor.apply();
+        String welcome = getString(R.string.welcome);
+        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+        Intent myIntent = new Intent(LoginActivity.this, Outstanding.class);
+        LoginActivity.this.startActivity(myIntent);
+
+    }
+
+    private void showLoginFailed() {
+        Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/cbr/cbrstaff/login/LoginViewModelFactory.java
@@ -0,0 +1,23 @@
+package com.cbr.cbrstaff.login;
+
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.support.annotation.NonNull;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel();
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
